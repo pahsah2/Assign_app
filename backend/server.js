728bd91f09bf9d826cc6d4e7e9335432f20c5470
pahsah2 <pahsah2@gmail.com>
2021-06-14 19:23:23 +0700

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use('/api/users', userRoutes)
 
 const __dirname = path.resolve()
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'customer') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
   app.get('*', (req, res) =>
